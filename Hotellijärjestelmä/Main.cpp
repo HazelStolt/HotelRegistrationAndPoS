741d043e0d44a55041e9c229a1550324a37ddbe5
HazelStolt <veeti.stolt@gmail.com>
2025-12-09 21:52:55 +0200

    Added more functionality, mainly more cases for checking how many rooms are available for reservation in case of inputting reserved room numbers. Also softlock prevention in program and other fixes + better comments.

--- Hotellijärjestelmä/Main.cpp
+++ Hotellijärjestelmä/Main.cpp
@@ -9,16 +9,17 @@
 
 using namespace std;
 
+//prototypes for the functions aside from "main"
 int order(int room[], int ROOMS, int PRICE);
 int menu(int room[], int ROOMS, int PRICE);
 
 
-int main() {
+int main() {	//main initializes constants and room reservation array
 
 	int const PRICE = 100;
-	int const ROOMS = 50;
+	int const ROOMS = 2;
 
-	int room[ROOMS]{};
+	int room[ROOMS];
 
 	menu(room, ROOMS, PRICE);
 
@@ -26,19 +27,20 @@ int main() {
 
 }
 
-int menu(int room[], int ROOMS, int PRICE) {
+int menu(int room[], int ROOMS, int PRICE) {	//menu handler
 
 	int thing;
 	int goAgain;
 	int sum = 0;
 	int free;
+	bool error;
 
-	cout << "\nWhat would you like to do?\n\n 1. Make reservation\n 2. Print current room reservation status\n 0. Close program\n\n";
+	cout << "\nWhat would you like to do?\n\n 1. Make reservation\n 2. Print current room reservation status\n 0. Close program\n\n";	//ask what user wants to do and take input 
 	cin >> thing;
 
-	switch (thing) {
+	switch (thing) {	//menu switch 
 
-	case 1: {
+	case 1:	{	//order case calls order function
 
 		order(room, ROOMS, PRICE);
 
@@ -50,12 +52,21 @@ int menu(int room[], int ROOMS, int PRICE) {
 			menu(room, ROOMS, PRICE);
 
 		}
+		else if (goAgain != 0) {
+
+			error = true;
+
+			cout << "Error! Please enter a supported input.";
+			cin.clear();
+			cin.ignore(80, '\n');
+
+		}
 
 		return 0;
 		break;
-		
+
 	}
-	case 2: {
+	case 2: {	//simple case prints out all rooms' reservation statuses
 
 		cout << "Current reservation status:\n\n";
 
@@ -65,6 +76,7 @@ int menu(int room[], int ROOMS, int PRICE) {
 
 				cout << "Room " << b + 1 << ". Reserved.\n";
 				sum++;
+
 			}
 			else {
 
@@ -86,27 +98,41 @@ int menu(int room[], int ROOMS, int PRICE) {
 
 		}
 
-		cout << "Would you like to exit the program?\n\n 1. Continue operating.  |  0. Close program.\n";
-		cin >> goAgain;
+		do {	//input validation for closing the program or continuing operation although this isn't very necessary, failing to input a number between 0 and 1 will just close the program without this input validator
 
-		if (goAgain == 1) {
+			error = false;
 
-			menu(room, ROOMS, PRICE);
+			cout << "Would you like to exit the program?\n\n 1. Continue operating.  |  0. Close program.\n";
+			cin >> goAgain;
 
-		}
-		return 0;
+			if (goAgain == 1) {
 
+				menu(room, ROOMS, PRICE);
+
+			}
+			else if (goAgain != 0) {
+
+				error = true;
+
+				cout << "Error! Please enter a supported input.";
+				cin.clear();
+				cin.ignore(80, '\n');
+
+			}
+
+			return 0;
+		} while (error);
 
 	}
-	case 0: {
+	case 0: {	//simply breaks the switch and returns 0 effectively ending the program
 
 		return 0;
 		break;
 
 	}
-	default: {
+	default: {	//default for making sure the input is allowed
 
-		cout << "\nUNSUPPORTED INPUT!\n\n";  //default for making sure the input is allowed
+		cout << "\nUNSUPPORTED INPUT!\n\n";  
 
 		cout << "Would you like to exit the program?\n\n 1. Continue operating.  |  0. Close program.\n";
 		cin >> goAgain;
@@ -123,7 +149,6 @@ int menu(int room[], int ROOMS, int PRICE) {
 	return 0;
 
 }
-
 
 int order(int room[], int ROOMS, int PRICE) {
 
@@ -167,7 +192,7 @@ int order(int room[], int ROOMS, int PRICE) {
 			cout << "Please input the wanted room number:\n\n";	//asking for wanted room number
 			cin >> roomNum;
 
-			if (cin.fail() || roomNum > ROOMS || roomNum < 1) {
+			if (cin.fail() || roomNum > ROOMS || roomNum < 1) {	//error state
 
 				error = true;
 				cout << "Error! Please input a valid number between 1-" << ROOMS << ".\n";
@@ -177,14 +202,42 @@ int order(int room[], int ROOMS, int PRICE) {
 			}
 			if (room[roomNum - 1] == 1) {	//this one to check whether the wanted room is occupied
 
-
 				error = true;
 				cout << "Sorry, this room already has a reservation.\nPlease input a new room number to continue.\n";
 				cin.clear();
 				cin.ignore(80, '\n');
 
-			}
+				int temp = 0;
 
+				for (int c = 0; c < ROOMS; c++) {	//something about softlock prevention 
+
+					if (room[c] == 0) {
+
+						temp++;
+
+					}
+					
+					cout << "\nThere are currently " << temp << " rooms available.\n";
+
+					if (temp == 0) {	//without this trying to make an order whilst all rooms are reserved would lock user in an endless loop of trying to reserve a room and failing due to all rooms being occupied.
+
+						cout << "\n\nNo rooms are currently available to reserve. Would you like to close the program?\n";
+						cin >> num;
+
+					}
+
+					if (num == 1) {
+
+						menu(room, ROOMS, PRICE);
+						error = false; 
+					}
+
+					break;
+
+				}
+
+			}
+			
 
 		} while (error);
 		
